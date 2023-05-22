c79edbda676e283b820ade99e64d8025a2e54b04
barkinbaydar <barkin.baydar@ug.bilkent.edu.tr>
2023-05-22 11:54:28 +0300

    Android Studio Files Added

---
+++ Bench_copy/app/src/main/java/com/sportech/bench/NewMatch.java
@@ -0,0 +1,14 @@
+package com.sportech.bench;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_match);
+    }
+}
