457da85020d5c240089f2f02efb5b47a3409ef4c
barkinbaydar <barkin.baydar@ug.bilkent.edu.tr>
2023-05-22 21:03:31 +0300

    The EditText objects in the NewMatch page are modified
    The functionality to keep the information in the EditText objects remain the same when the page is changed from the Menu Bar and clear the information written in these objects when the cancelButton is clicked.

--- Bench_Design/app/src/main/java/com/sportech/bench/NewMatch.java
+++ Bench_Design/app/src/main/java/com/sportech/bench/NewMatch.java
@@ -3,6 +3,7 @@ package com.sportech.bench;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,8 @@ public class NewMatch extends AppCompatActivity {
         private ImageButton profileButton, matchesButton;
         private Button cancelButton;
         private EditText name, day, month, year, hour, min, adress, playerNo;
-        private String matchName, matchDate, matchTime, matchAdress, playersNeeded;
+        private String matchName, matchDay, matchMonth, matchYear, matchHour, matchMinute, matchAdress, playersNeeded;
+        private static boolean cancelClicked;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +33,7 @@ public class NewMatch extends AppCompatActivity {
         hour = findViewById(R.id.hourInput);
         min = findViewById(R.id.minuteInput);
         adress = findViewById(R.id.adressInput);
+
         playerNo = findViewById(R.id.playerNoInput);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +60,7 @@ public class NewMatch extends AppCompatActivity {
     //Menu Bar Button Methods
     protected void goToProfilePage()
     {
+        playersNeeded = playerNo.getText().toString();
         Intent profileIntent = new Intent(NewMatch.this, Profile.class);
         finish();
         startActivity(profileIntent);
@@ -64,19 +68,90 @@ public class NewMatch extends AppCompatActivity {
 
     protected void goToMatchesPage()
     {
-        Intent matcesIntent = new Intent(NewMatch.this, MatchesActivity.class);
+        playersNeeded = playerNo.getText().toString();
+        Intent matchesIntent = new Intent(NewMatch.this, MatchesActivity.class);
         finish();
-        startActivity(matcesIntent);
+        startActivity(matchesIntent);
     }
 
     //Page Button Methods
     protected void cancelCreating()
     {
-        goToMatchesPage();
+        SharedPreferences preferences = getSharedPreferences("MatchInformation", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
+
+        name.setText("");
+        day.setText("");
+        month.setText("");
+        year.setText("");
+        hour.setText("");
+        min.setText("");
+        adress.setText("");
+        playerNo.setText("");
+
+        Intent cancelIntent = new Intent(NewMatch.this, MatchesActivity.class);
+        finish();
+        startActivity(cancelIntent);
     }
 
     protected void confirmCreating()
     {
 
     }
+
+    //methods to prevent the texts inside the editTexts to disappear when the activity is changed
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        matchName = name.getText().toString();
+        matchDay = day.getText().toString();
+        matchMonth = month.getText().toString();
+        matchYear = year.getText().toString();
+        matchHour = hour.getText().toString();
+        matchMinute = min.getText().toString();
+        matchAdress = adress.getText().toString();
+        playersNeeded = playerNo.getText().toString();
+
+        SharedPreferences preferences = getSharedPreferences("MatchInformation", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name", matchName);
+        editor.putString("day", matchDay);
+        editor.putString("month", matchMonth);
+        editor.putString("year", matchYear);
+        editor.putString("hour", matchHour);
+        editor.putString("minute", matchMinute);
+        editor.putString("adress", matchAdress);
+        editor.putString("player", playersNeeded);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        SharedPreferences preferences = getSharedPreferences("MatchInformation", MODE_PRIVATE);
+        matchName = preferences.getString("name","");
+        matchDay = preferences.getString("day","");
+        matchMonth = preferences.getString("month","");
+        matchYear = preferences.getString("year","");
+        matchHour = preferences.getString("hour","");
+        matchMinute = preferences.getString("minute","");
+        matchAdress = preferences.getString("adress","");
+        playersNeeded = preferences.getString("player","");
+
+        name.setText(matchName);
+        day.setText(matchDay);
+        month.setText(matchMonth);
+        year.setText(matchYear);
+        hour.setText(matchHour);
+        min.setText(matchMinute);
+        adress.setText(matchAdress);
+        playerNo.setText(playersNeeded);
+    }
+
 }
