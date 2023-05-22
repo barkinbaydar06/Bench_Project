2a3a9e7eea49f9d517de2dd5896617f67fb4cf8b
barkinbaydar <barkin.baydar@ug.bilkent.edu.tr>
2023-05-22 18:46:40 +0300

    New Match Page Cancel Button Functionality added

--- Bench_Design/app/src/main/java/com/sportech/bench/NewMatch.java
+++ Bench_Design/app/src/main/java/com/sportech/bench/NewMatch.java
@@ -5,17 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class NewMatch extends AppCompatActivity {
 
         private ImageButton profileButton, matchesButton;
+        private Button cancelButton;
+        private EditText name, day, month, year, hour, min, adress, playerNo;
+        private String matchName, matchDate, matchTime, matchAdress, playersNeeded;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_match);
         profileButton = findViewById(R.id.profileButtonCreate);
         matchesButton = findViewById(R.id.matchesButtonCreate);
+
+        cancelButton = findViewById(R.id.cancelButton);
+
+        name = findViewById(R.id.matchNameInput);
+        day = findViewById(R.id.dayInput);
+        month = findViewById(R.id.monthInput);
+        year = findViewById(R.id.yearInput);
+        hour = findViewById(R.id.hourInput);
+        min = findViewById(R.id.minuteInput);
+        adress = findViewById(R.id.adressInput);
+        playerNo = findViewById(R.id.playerNoInput);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
@@ -28,6 +44,12 @@ public class NewMatch extends AppCompatActivity {
             public void onClick(View v)
             {
                 goToMatchesPage();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelCreating();
             }
         });
     }
@@ -45,5 +67,16 @@ public class NewMatch extends AppCompatActivity {
         Intent matcesIntent = new Intent(NewMatch.this, MatchesActivity.class);
         finish();
         startActivity(matcesIntent);
+    }
+
+    //Page Button Methods
+    protected void cancelCreating()
+    {
+        goToMatchesPage();
+    }
+
+    protected void confirmCreating()
+    {
+
     }
 }
